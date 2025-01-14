307c3881ceb30ab303bfd6785afbcda011285504
zarapatel117 <zarapatel2510@gmail.com~>
2025-01-14 20:51:59 +0530

    random date

---
+++ Random date and time.py
@@ -0,0 +1,14 @@
+import random
+import time
+def getrandomdate(sd,ed):
+    print("print random date between",sd,"and",ed )
+    randomgen=random.random()
+    dateformat='%m/%d/%Y'
+    st=time.mktime(time.strptime(sd,dateformat))
+    et=time.mktime(time.strptime(ed,dateformat))
+
+    randomtime=st+randomgen*(et-st)
+    randomdate=time.strftime(dateformat, time.localtime(randomtime))
+    return randomdate
+print("randomdate=",getrandomdate("1/1/2016","12/2/2018"))
+
